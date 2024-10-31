dde05cfdc8e0a8e49b6005d7ee8316bb61d62f45
Timo654 <41972888+Timo654@users.noreply.github.com>
2024-10-31 12:46:19 +0200

    add particles

--- Assets/Scripts/PegObject.cs
+++ Assets/Scripts/PegObject.cs
@@ -12,15 +12,21 @@ public class PegObject : MonoBehaviour
     [SerializeField] Color bonusColor = Color.magenta;
     [SerializeField] private PegType pegType; // maybe randomize these
     private SpriteRenderer spriteRenderer;
+    private ParticleSystem particles;
+    private ParticleSystem.MainModule ma;
     private bool popped = false;
     // Start is called before the first frame update
     private void Awake()
     {
         spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        particles = GetComponent<ParticleSystem>();
+        ma = particles.main;
+        ma.startColor = normalColor;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (popped) return;
+        particles.Play();
         OnPegPopped?.Invoke(pegType);
         popped = true;
         spriteRenderer.DOFade(0f, 0.5f).OnComplete(() => gameObject.SetActive(false));
@@ -32,23 +38,28 @@ public class PegObject : MonoBehaviour
     }
     public void SetPegType(PegType pegType)
     {
-        
+
         this.pegType = pegType;
         switch (pegType)
         {
             case PegType.Normal:
                 spriteRenderer.color = normalColor;
+                ma.startColor = normalColor;
                 break;
             case PegType.Bonus:
                 spriteRenderer.color = bonusColor;
+                ma.startColor = bonusColor;
                 break;
             case PegType.Red:
                 spriteRenderer.color = redColor;
+                ma.startColor = redColor;
                 break;
             case PegType.Special:
                 spriteRenderer.color = specialColor;
+                ma.startColor = specialColor;
                 break;
             default:
+                Debug.Log($"Unknown peg type {pegType}");
                 break;
         }
     }
